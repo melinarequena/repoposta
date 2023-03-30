2bc51a7345b7bcca5d7768b179d4dce5e32ad211
Melina <mrequena@fue.undef.edu.ar>
2023-03-30 18:55:48 -0300

    Ingresado un texto mostrar cuantas a tiene

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!!!\n");
+// //INGRESADO UN TEXTO MOSTRAR COMO RESULTADO LA CANTIDAD DE VECES QUE APARECIO LA LETRA A,
+#include<stdio.h>
+int main()
+{
+    char texto[100];
+    int i, cont=0;
+    printf("Ingrese un texto:\n");
+    gets(texto);
+    for(i=0; i<100; i++)
+    {
+        if (texto[i]=='A' || texto[i]=='a')
+            cont++;
+    }
+    printf("La cantidad de veces que aparece la letra a en el texto es de %d", cont);
     return 0;
 }
