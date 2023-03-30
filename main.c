291fb476696311f2d93480939c21861977fde23e
Melina <mrequena@fue.undef.edu.ar>
2023-03-30 19:06:43 -0300

    Cuantas vocales y consonantes tiene

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
-// //INGRESADO UN TEXTO MOSTRAR COMO RESULTADO LA CANTIDAD DE VECES QUE APARECIO LA LETRA A,
+// /*Ingresar un texto y contar cuántas vocales y consonantes hay. Considerar las mayúsculas
+//y las minúsculas iguales*/
 #include<stdio.h>
 int main()
 {
     char texto[100];
-    int i, cont=0;
-    printf("Ingrese un texto:\n");
+    int contv=0, contc=0, i;
+    printf("Ingrese un texto");
+    fflush(stdin);
     gets(texto);
-    for(i=0; i<100; i++)
+    for(i=0; texto[i]!='\0'; i++)
     {
-        if (texto[i]=='A' || texto[i]=='a')
-            cont++;
+        if(texto[i]=='a'|| texto[i]=='e'|| texto[i]=='i'|| texto[i]=='o'|| texto[i]=='u'|| texto[i]=='A'||texto[i]=='E'||texto[i]=='I'||texto[i]=='O'||texto[i]=='U')
+        contv++;
+        else
+            contc++;
     }
-    printf("La cantidad de veces que aparece la letra a en el texto es de %d", cont);
+    printf("La cantidad de vocales presentes en el texto es de %d, mientras que la cantidad de consonantes es de %d", contv, contc);
+
     return 0;
+
 }
